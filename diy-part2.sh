459411149db61949fa4231d2b02827230dcefca9
zjss0415 <63588477+zjss0415@users.noreply.github.com>
2020-12-12 15:27:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/CHN-beta/xmurp-ua.git package/xmurp-ua
+git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
